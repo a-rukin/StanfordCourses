66c4e25c96b775065867e0e65b5258c231a7b256
Alexander Rukin <a-rukin@yandex-team.ru>
2013-11-10 16:05:49 +0400

    Deck class

--- Cards/Cards/Card.h
+++ Cards/Cards/Card.h
@@ -3,7 +3,7 @@
 //
 
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 
 @interface Card : NSObject
---
+++ Cards/Cards/Deck.h
@@ -0,0 +1,17 @@
+//
+// Created by Alexander Rukin on 10.11.13.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+- (void)addCard:(Card *)card;
+
+- (Card *)drawRandomCard;
+
+@end
